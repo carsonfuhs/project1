602c2e2d6c5d06fbf3efcc584fc873d8bb1a3ded
Carson Fuhs <c.fuhs3@gmail.com>
2021-02-27 17:42:24 -0600

    Added some initial Javascript. Haven't really done anything yet, just naming variables for API tokens and researching the API behavior.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+//API url: https://finnhub.io/api/v1/
+//c0ta3b748v6r4maenot0
+
+const apiURL = "https://finnhub.io/api/v1/"
+const apiKey = "&token=c0ta3b748v6r4maenot0" //includes token value - attached to end of each API hit
+
+$.ajax({
+    url: apiURL +
+})
+
+//console.log(apiURL + "quote?symbol=AAPL" + apiKey)
