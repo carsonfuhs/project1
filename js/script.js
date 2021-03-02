2eaffff82571ba226f509709ee85f00821d1cec6
Carson Fuhs <c.fuhs3@gmail.com>
2021-03-01 23:59:14 -0600

    Added lots of new JS. Started working on the callback function and async. Finally got parts of the JQuery working.

--- js/script.js
+++ js/script.js
@@ -6,9 +6,58 @@
 const apiURL = "https://finnhub.io/api/v1/"
 const apiKey = "&token=c0ta3b748v6r4maenot0" //includes token value - attached to end of each API hit
 
-$.ajax({
-    url: apiURL +
-})
+
+
+// $.ajax({
+//     url: apiURL + 
+// })
 
 //console.log(apiURL + "quote?symbol=AAPL" + apiKey)
 // hello
+
+//const submitButton = $("#submitButton")
+
+// const submitButton = document.querySelector("#submitButton");
+// //let userTickerInput, testVar;
+
+// submitButton.addEventListener("click", (event) => {
+//     console.log(event);
+//     console.log("success");
+//     const userTickerInput = document.getElementById("tickerInput").value;
+//     console.log(userTickerInput);
+//     let testVar = 4;
+//     console.log(testVar)
+// });
+// let user;
+
+///let userTickerInput;
+
+// function getTicker() {
+//     userTickerInput = $("tickerInput").val();
+// }
+
+$("#submitButton").on("click", function(event) {
+    event.preventDefault(); //prevents page refresh
+    //console.log(event);
+    
+    const userTickerInput = $("#tickerInput").val();
+
+    //gets a stock quote
+    $.ajax({
+        url: apiURL + "quote?symbol=" + userTickerInput + apiKey
+    }).then(
+        (data) => {
+            console.log(data)
+        },
+        (error) => {
+            console.log("error")
+        }
+    )
+    
+    
+    //console.log(userTickerInput)
+});
+
+
+
+// //console.log(userInput)
