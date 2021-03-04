d8906ef8b68dfda0a23d06468e730ab307a8f003
Carson Fuhs <c.fuhs3@gmail.com>
2021-03-04 11:32:20 -0600

    Added JQuery $.when to accomodate for multiple API hits

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
 
-//API url: https://finnhub.io/api/v1/
+//API url: https://finnhub.io/api/v1/quote?symbol=AAPL&token=c0ta3b748v6r4maenot0
+//https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2020-04-30&to=2020-05-01&token=c0ta3b748v6r4maenot0
+//company-news?symbol=AAPL&from=2020-04-30&to=2020-05-01
 //c0ta3b748v6r4maenot0
 
 const apiURL = "https://finnhub.io/api/v1/"
@@ -38,26 +40,82 @@ const apiKey = "&token=c0ta3b748v6r4maenot0" //includes token value - attached t
 
 $("#submitButton").on("click", function(event) {
     event.preventDefault(); //prevents page refresh
-    //console.log(event);
-    
+    $(".stock-price").remove() //clears page if there's other stock details
+    $(".stock-news-headline").remove()
+
     const userTickerInput = $("#tickerInput").val();
 
-    //gets a stock quote
-    $.ajax({
-        url: apiURL + "quote?symbol=" + userTickerInput + apiKey
-    }).then(
-        (data) => {
-            console.log(data)
+    $.when(
+        $.ajax({url: apiURL + "quote?symbol=" + userTickerInput + apiKey}),
+        $.ajax({url: apiURL + "company-news?symbol=" + userTickerInput + "&from=2021-01-01&to=2021-03-03" + apiKey}),
+    ).then(
+        (data1, data2) => {
+            console.log(data1[0].c)
+            console.log(data2[0][0])
+            //behavior of the API is all 0's when an invalid ticker is sent. This catches it.
+            if (data1[0].l === 0 & data1[0].h === 0 & data1[0].c === 0) {
+                console.log("that ticker doesn't exist")
+            } else {
+                const lastPrice = data1[0].c;
+                let $stock = $("<h2>").addClass("stock-price");
+                $("body").append($stock);
+                $(".stock-price").html('Price: ' + lastPrice)
+
+                let newsHeadline = data2[0][0].headline;
+                console.log(newsHeadline)
+                let $newsHeadline = $("<h2>").addClass("stock-news-headline");
+                $("body").append($newsHeadline);
+                $(".stock-news-headline").html('Latest Headline: ' + newsHeadline)
+            }
         },
-        (error) => {
+        (error1) => {
             console.log("error")
         }
     )
+
+
+
+
+    // //gets the stock price
+    // $.ajax({
+    //     url: apiURL + "quote?symbol=" + userTickerInput + apiKey
+    // }).then(
+    //     (data) => {
+    //         console.log(data)
+    //         //behavior of the API is all 0's when an invalid ticker is sent. This catches it.
+    //         if (data.l === 0 & data.h === 0 & data.l === 0) {
+    //             console.log("that ticker doesn't exist")
+    //         } else {
+    //             const lastPrice = data.l;
+    //             let $stock = $("<h2>").addClass("stock-price");
+    //             $("body").append($stock);
+    //             $(".stock-price").html('Price: ' + lastPrice)
+    //         }
+    //     },
+    //     (error) => {
+    //         console.log("error")
+    //     }
+    // )
     
+    // $.ajax({
+    //     url: apiURL + "company-news?symbol=" + userTickerInput + "&from=2021-01-01&to=2021-03-03" + apiKey
+    // }).then(
+    //     (data2) => {
+    //         //console.log(data2)
+    //         let newsHeadline = data2[0].headline;
+    //         console.log(newsHeadline)
+    //         let $newsHeadline = $("<h2>").addClass("stock-news-headline");
+    //         $("body").append($newsHeadline);
+    //         $(".stock-news-headline").html('Latest Headline: ' + newsHeadline)
+    //     },
+
+    //     (error) => {
+    //         console.log("error")
+    //     }
+
+    // )
     
-    //console.log(userTickerInput)
 });
 
 
 
-// //console.log(userInput)
