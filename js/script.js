21433655a457fcc7d37ea4a8f9b7819cccc6eb4e
Carson Fuhs <c.fuhs3@gmail.com>
2021-03-05 22:20:01 -0600

    Final project, basically done. Lots of things to add and refactor in the future but this is fine as it is.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
-
-
-//API url: https://finnhub.io/api/v1/quote?symbol=AAPL&token=c0ta3b748v6r4maenot0
-//https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2020-04-30&to=2020-05-01&token=c0ta3b748v6r4maenot0
-//company-news?symbol=AAPL&from=2020-04-30&to=2020-05-01
-//c0ta3b748v6r4maenot0
-
 const apiURL = "https://finnhub.io/api/v1/"
 const apiKey = "&token=c0ta3b748v6r4maenot0" //includes token value - attached to end of each API hit
 
 $("#submitButton").on("click", function(event) {
-    event.preventDefault(); //prevents page refresh
+    event.preventDefault();
 
     //clears page if there's other stock details
     $(".stock-price").remove() 
@@ -17,6 +10,10 @@ $("#submitButton").on("click", function(event) {
     $(".stock-news-list").remove()
     $(".ticker-error").remove()
     $(".company-profile").remove()
+    $(".analyst-recommendation-title").remove()
+    $(".analyst-recommendation-list").remove()
+    $(".company-profile").remove()
+    $(".company-website").remove()
 
     //grabs user input, makes it uppercase
     let userTickerInput = $("#tickerInput").val();
@@ -33,8 +30,9 @@ $("#submitButton").on("click", function(event) {
         $.ajax({url: apiURL + "quote?symbol=" + userTickerInput + apiKey}),
         $.ajax({url: apiURL + "company-news?symbol=" + userTickerInput + "&from=2021-01-25&to=2021-03-03" + apiKey}),
         $.ajax({url: apiURL + "stock/profile2?symbol=" + userTickerInput + apiKey}),
+        $.ajax({url: apiURL + "stock/recommendation?symbol=" + userTickerInput + apiKey}),
     ).then(
-        (quote, companyNews, companyProfile) => {
+        (quote, companyNews, companyProfile, analystRecommendations) => {
 
             //behavior of the API is all 0's when an invalid ticker is sent. This catches it.
             //not the best solution but will update in the future.
@@ -47,31 +45,48 @@ $("#submitButton").on("click", function(event) {
             //company information
                 const companyName = companyProfile[0].name;
                 const companyWebsite = companyProfile[0].weburl;
-                let $companyProfile = $("<h2>").addClass("company-profile");
-                let $companyWebsite = $("<h2>").addClass("company-website");
-                $("body").append($companyProfile);
-                $("body").append($companyWebsite);
-                $(".company-profile").html("Company: " + companyName)
+                let $companyProfile = $("<h1>").addClass("company-profile");
+                let $companyWebsite = $("<h4>").addClass("company-website");
+                
+                $("form").after($companyProfile);
+                $companyProfile.after($companyWebsite);
+                $(".company-profile").html(companyName)
                 $(".company-website").html("<a href=" + companyWebsite + " target=_blank>" + companyWebsite)
 
             //shows last stock price
                 const lastPrice = quote[0].c;
-                let $stock = $("<h2>").addClass("stock-price");
-                $("body").append($stock);
+                let $stock = $("<h3>").addClass("stock-price");
+                $companyWebsite.after($stock);
                 $(".stock-price").html('Last share price:  $' + lastPrice)
 
             //creates items on DOM
                 $newsTitle = $("<h2>").addClass("stock-news-title");
                 $newsList = $("<ul>").addClass("stock-news-list");
-                $("body").append($newsTitle);
-                $("body").append($newsList);
+
+                $(".news").append($newsTitle);
+                $(".news").append($newsList);
                 $(".stock-news-title").html('Related News Headlines:')
 
                 //loops through news array, takes most recent 5, adds as unordered list, links open in new tab
                 for (let i = 0; i < 10; i++) {
-                    console.log(companyNews[0][i])
                     $(".stock-news-list").append("<li><a href=" + companyNews[0][i].url + " target=_blank>" + companyNews[0][i].headline + "</li>")
                 }
+            
+            //gets analyst recommendations
+                const ar = analystRecommendations;
+                $analystRecommendation = $("<h2>").addClass("analyst-recommendation-title");
+                $analystRecommendationList = $("<ul>").addClass("analyst-recommendation-list");
+                $(".recs").append($analystRecommendation);
+                $(".recs").append($analystRecommendationList);
+                $(".analyst-recommendation-title").html('Analyst Recommendations as of ' + ar[0][0].period) //period is time period for recs
+                
+                //adds analyst recs to page
+                $(".analyst-recommendation-list").append("<li>Strong Buy: " + ar[0][0].strongBuy+ "</li>")
+                $(".analyst-recommendation-list").append("<li>Buy: " + ar[0][0].buy + "</li>")
+                $(".analyst-recommendation-list").append("<li>Hold: " + ar[0][0].hold + "</li>")
+                $(".analyst-recommendation-list").append("<li>Sell: " + ar[0][0].sell + "</li>")
+                $(".analyst-recommendation-list").append("<li>Strong Sell: " + ar[0][0].strongSell+ "</li>")
+                
 
             }
 
